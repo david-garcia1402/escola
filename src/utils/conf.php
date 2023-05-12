be8cbad43f24e8a3cd26a26730b10c2f22e97e2e
Valmir F <120138460+david-garcia1402@users.noreply.github.com>
2023-05-12 15:34:31 -0300

    exclusão de arquivos

--- src/utils/conf.php
+++
@@ -1,6 +0,0 @@
-$conf = array(
-        'dbHost'     => 'localhost',
-        'dbDatabase' => 'escola', //DADOS DO BANCO
-        'dbUser'     => 'escola', //DADOS DO BANCO
-        'dbPass'     => 'escola', //DADOS DO BANCO
-      );
