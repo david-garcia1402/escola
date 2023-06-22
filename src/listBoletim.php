0cb98fe715ac57b513dd0ca667380cb2e612a367
Valmir F <120138460+david-garcia1402@users.noreply.github.com>
2023-06-22 14:25:59 -0300

    fix: notas do boletim
    fix: agora está mostrando o valor zero com default

--- src/listBoletim.php
+++ src/listBoletim.php
@@ -1,6 +1,6 @@
 <?php 
     require("utils/conection.php");
-    
+    ini_set('display_errors', 1);
     $idAluno = $_GET["idAluno"];
     
     $sqlaluno = "select id, nome, turno from alunos where id = '{$idAluno}'";
@@ -12,13 +12,28 @@
     $retorno['turno']    = $user_data['turno'];
     $retorno['materias'] = array();
         
-    $sql = "select alunos.id as 'id do aluno', alunos.nome as 'aluno', materias.nome as 'materia', boletim.b1notas, boletim.b2notas, boletim.b3notas 
+    $sql = "select alunos.id as 'idaluno', alunos.nome as 'idmat', materias.nome, boletim.b1notas, boletim.b2notas, boletim.b3notas 
             from boletim, alunos, materias 
             where boletim.idAluno = alunos.id and boletim.idMate = materias.id 
-            order by alunos.id asc;;";
-    $res = SmtConnection::getQuery($sql);
-      
-    while($row = mysqli_fetch_row($res)) {
+            order by alunos.id asc;";
+    $ressql = SmtConnection::getQuery($sql);
+    $res = mysqli_num_rows($ressql);
+    if ($res == 0) {
+        $sqlmat = "select id, nome from materias";
+        $resultmat = SmtConnection::getQuery($sqlmat);
+        while ($vetmat = mysqli_fetch_assoc($resultmat)) {
+            $idMateria = $vetmat['id'];
+            $sqlnotas = "insert into boletim (b1notas, b2notas, b3notas, idMate, idAluno) values (0, 0, 0, '{$idMateria}' , '{$idAluno}')";
+            $res = SmtConnection::getQuery($sqlnotas);
+        }        
+    }
+
+    $sql2 = "select alunos.id as 'idaluno', alunos.nome as 'idmat', materias.nome, boletim.b1notas, boletim.b2notas, boletim.b3notas 
+    from boletim, alunos, materias 
+    where boletim.idAluno = alunos.id and boletim.idMate = materias.id 
+    order by alunos.id asc;";
+    $res2 = SmtConnection::getQuery($sql2);
+    while($row = mysqli_fetch_row($res2)) {
         $materia = array(
             "id" => $row[0],
             "nome" => $row[2],
